59274d6762bd20a9be2578b686b591ed29d4f7a9
caiiiou <jamesma909gmail.com>
2026-02-09 02:01:11 -0500

    added more test cases

--- src/InCollege.cob
+++ src/InCollege.cob
@@ -195,11 +195,10 @@ IDENTIFICATION DIVISION.
            END-EVALUATE.
 
        RegisterUser.
-           IF UserCount > 5
+           IF UserCount >= 5
                DISPLAY "All permitted accounts have been created, please
 come back later"
-               PERFORM InitialMenu
-               EXIT PARAGRAPH
+               GO TO InitialMenu
            END-IF
            MOVE "Enter username:" TO CurrentMessage
            PERFORM DisplayAndLog
